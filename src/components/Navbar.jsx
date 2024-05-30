666fa4449642b20dc5f681978a9e4828fafc59c4
vzn <ilyaromensky@gmail.com>
2024-05-30 15:24:53 +0300

    master

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from "react"
+import styled from "styled-components"
+import SearchIcon from '@mui/icons-material/Search';
+import { Badge } from '@mui/material';
+import { ShoppingCartOutlined } from '@mui/icons-material';
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #ffffff;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Left = styled.div`
+    flex:1;
+    text-align: center;
+`
+
+const Center = styled.div`
+    flex:1;
+    text-align: center;
+    align-items: center;
+`
+
+const Right = styled.div`
+    flex:1;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    align-items: center;
+    display: flex;
+    padding: 5px;
+    width: fit-content;
+`
+
+const Input = styled.input`
+    border: none;
+`
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-right: 30px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <SearchContainer><SearchIcon style={{color: "gray", fontSize:16}}/><Input></Input></SearchContainer>
+            </Left>
+            <Center><Logo>STORE.</Logo></Center>
+            <Right>
+                <MenuItem>Регистрация</MenuItem>
+                <MenuItem>Войти</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined/>
+                    </Badge>
+                </MenuItem>
+            </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Line from "../components/Line";
+
+const Home = () => {
+  return (
+    <div>
+        <Line/>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
